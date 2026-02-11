4212374886b04cc481de0dd803cfe42ea2dc8172
raphael <ellrapha.ferreira@gmail.com>
2026-02-11 11:43:50 -0300

    aula-06.2

--- aula-06/script.js
+++ aula-06/script.js
@@ -1,14 +1,23 @@
-function somar (){
-    let numero1 = Number(document.getElementById("n1").value)
-    let numero2 = Number(document.getElementById("n2").value)
+// function somar (){
+//     let numero1 = Number(document.getElementById("n1").value)
+//     let numero2 = Number(document.getElementById("n2").value)
 
-    let resposta = document.getElementById("resposta")
-    // console.log(numero1 + numero2)
-    let soma = numero1 + numero2
-    resposta.innerHTML = soma
-}
-function altimg (){
-    let imagem = document.getElementById("imga")
+//     let resposta = document.getElementById("resposta")
+//     // console.log(numero1 + numero2)
+//     let soma = numero1 + numero2
+//     resposta.innerHTML = soma
+// }
+// function altimg (){
+//     let imagem = document.getElementById("imga")
 
-    imagem.setAttribute("src","https://clicr.com.br/wp-content/uploads/2023/01/linguicinha-pet-1.jpg")
+//     imagem.setAttribute("src","https://clicr.com.br/wp-content/uploads/2023/01/linguicinha-pet-1.jpg")
+// }
+
+let square = document.getElementById("q1")
+let corx = document.getElementById("cor")
+function cor(e){
+    let color = e.target.value
+    square.style.backgroundColor = color
 }
+
+corx.addEventListener("input",cor)
